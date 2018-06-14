db5aaf570074f2d77a013a468fa9f9af9642133c
yunlong.su <yunlong.su@baidao.com>
2018-06-14 17:48:50 +0800

    modify activity

--- apk-build/multidexbuild/src/main/java/com/sososeen09/multidexbuild/MainActivity.java
+++ apk-build/multidexbuild/src/main/java/com/sososeen09/multidexbuild/MainActivity.java
@@ -36,62 +36,62 @@ public class MainActivity extends Activity {
         tvResult.setText("show the result: " + SimpleMathUtils.divide());
     }
 
-public void fix(View view) {
-    tvFix.setText("fixing...");
-    File originDex = null;
-    try {
-        originDex = copyFileFromAssets("fixed.dex", getCacheDir().getAbsolutePath());
-    } catch (IOException e) {
-        e.printStackTrace();
-        Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
-        return;
-    }
-
-    if (originDex != null) {
-        File dexOptimizeDir = getDir("dex", Context.MODE_PRIVATE);
-        String dexOutputPath = dexOptimizeDir.getAbsolutePath();
-        PathClassLoader pathClassLoader = (PathClassLoader) getClassLoader();
-        DexClassLoader dexClassLoader = new DexClassLoader(originDex.getAbsolutePath(), dexOutputPath, null,
-                pathClassLoader);
-
+    public void fix(View view) {
+        tvFix.setText("fixing...");
+        File originDex = null;
         try {
-            // 获取DexClassLoader对象的pathList对象，DexPathList
-            Object dexPathListWithDexClassLoader = ReflectUtils.findField(dexClassLoader, "pathList").get(dexClassLoader);
-
-            // 获取DexPathList对象Element[]数组，对应的字段名是dexElements
-            Field dexElements = ReflectUtils.findField(dexPathListWithDexClassLoader, "dexElements");
-            Object[] elements = (Object[]) dexElements.get(dexPathListWithDexClassLoader);
-
-            // 获取PathClassLoader对象的pathList对象，DexPathList
-            Object dexPathListWithPathClassLoader = ReflectUtils.findField(pathClassLoader, "pathList").get(pathClassLoader);
-
-            //把之前获取的Element[]数组插入到PathClassLoader对象对应的DexPathList的Element数组中
-            ReflectUtils.insertFieldArray(dexPathListWithPathClassLoader, "dexElements", elements);
-
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
+            originDex = copyFileFromAssets("fixed.dex", getCacheDir().getAbsolutePath());
+        } catch (IOException e) {
             e.printStackTrace();
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            return;
         }
 
+        if (originDex != null) {
+            File dexOptimizeDir = getDir("dex", Context.MODE_PRIVATE);
+            String dexOutputPath = dexOptimizeDir.getAbsolutePath();
+            PathClassLoader pathClassLoader = (PathClassLoader) getClassLoader();
+            DexClassLoader dexClassLoader = new DexClassLoader(originDex.getAbsolutePath(), dexOutputPath, null,
+                    pathClassLoader);
+
+            try {
+                // 获取DexClassLoader对象的pathList对象，DexPathList
+                Object dexPathListWithDexClassLoader = ReflectUtils.findField(dexClassLoader, "pathList").get(dexClassLoader);
+
+                // 获取DexPathList对象Element[]数组，对应的字段名是dexElements
+                Field dexElements = ReflectUtils.findField(dexPathListWithDexClassLoader, "dexElements");
+                Object[] elements = (Object[]) dexElements.get(dexPathListWithDexClassLoader);
+
+                // 获取PathClassLoader对象的pathList对象，DexPathList
+                Object dexPathListWithPathClassLoader = ReflectUtils.findField(pathClassLoader, "pathList").get(pathClassLoader);
+
+                //把之前获取的Element[]数组插入到PathClassLoader对象对应的DexPathList的Element数组中
+                ReflectUtils.insertFieldArray(dexPathListWithPathClassLoader, "dexElements", elements);
+
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+        }
+
+        tvFix.setText("done!");
     }
 
-    tvFix.setText("done!");
-}
+    private File copyFileFromAssets(String assetName, String dexOutputDir) throws IOException {
+        File originDex = null;
+        AssetManager assets = getAssets();
+        InputStream open = assets.open(assetName);
+        originDex = new File(dexOutputDir, assetName);
+        FileOutputStream fileOutputStream = new FileOutputStream(originDex);
+        byte[] bytes = new byte[1024];
+        int len = 0;
+        while ((len = open.read(bytes)) != -1) {
+            fileOutputStream.write(bytes, 0, len);
+        }
+        fileOutputStream.close();
+        open.close();
 
-private File copyFileFromAssets(String assetName, String dexOutputDir) throws IOException {
-    File originDex = null;
-    AssetManager assets = getAssets();
-    InputStream open = assets.open(assetName);
-    originDex = new File(dexOutputDir, assetName);
-    FileOutputStream fileOutputStream = new FileOutputStream(originDex);
-    byte[] bytes = new byte[1024];
-    int len = 0;
-    while ((len = open.read(bytes)) != -1) {
-        fileOutputStream.write(bytes, 0, len);
+        return originDex;
     }
-    fileOutputStream.close();
-    open.close();
-
-    return originDex;
-}
 }
