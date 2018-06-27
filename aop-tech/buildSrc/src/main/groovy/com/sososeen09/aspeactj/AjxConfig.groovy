80d5aa014e39dc18a484a9208c9d23ac0a573da2
yunlong.su <yunlong.su@baidao.com>
2018-06-27 18:00:06 +0800

    ajx task

---
+++ aop-tech/buildSrc/src/main/groovy/com/sososeen09/aspeactj/AjxConfig.groovy
@@ -0,0 +1,51 @@
+package com.sososeen09.aspeactj
+
+import com.android.build.gradle.AppPlugin
+import com.android.build.gradle.BasePlugin
+import com.android.build.gradle.LibraryPlugin
+import com.android.build.gradle.api.BaseVariant
+import org.gradle.api.DomainObjectCollection
+import org.gradle.api.GradleException
+import org.gradle.api.Project
+
+class AjxConfig {
+
+    private final Project project
+    private final boolean hasAppPlugin
+    private final boolean hasLibPlugin
+    private final BasePlugin plugin
+
+    AjxConfig(Project project) {
+        this.project = project
+        this.hasAppPlugin = project.plugins.hasPlugin(AppPlugin)
+        this.hasLibPlugin = project.plugins.hasPlugin(LibraryPlugin)
+
+        if (!hasAppPlugin && !hasLibPlugin) {
+            throw new GradleException("android-aspjectjx: The 'com.android.application' or 'com.android.library' plugin is required.")
+        }
+
+        //AppPlugin 和LibraryPlugin 均继承自BasePlugin
+        this.plugin = project.plugins.getPlugin(hasAppPlugin ? AppPlugin : LibraryPlugin)
+    }
+
+    /**
+     * Return all variants.
+     *
+     * @return Collection of variants.
+     */
+    DomainObjectCollection<BaseVariant> getVariants() {
+        return hasAppPlugin ? project.android.applicationVariants : project.android.libraryVariants
+    }
+
+    /**
+     * Return boot classpath.
+     * @return Collection of classes.
+     */
+    List<File> getBootClasspath() {
+        if (project.android.hasProperty('bootClasspath')) {
+            return project.android.bootClasspath
+        } else {
+            return plugin.runtimeJarList
+        }
+    }
+}
--- aop-tech/buildSrc/src/main/groovy/com/sososeen09/aspeactj/AjxPlugin.groovy
+++ aop-tech/buildSrc/src/main/groovy/com/sososeen09/aspeactj/AjxPlugin.groovy
@@ -1,9 +1,6 @@
 package com.sososeen09.aspeactj
 
-import com.android.build.gradle.AppExtension
 import com.android.build.gradle.AppPlugin
-import com.android.build.gradle.LibraryPlugin
-import com.android.build.gradle.api.ApplicationVariant
 import org.aspectj.bridge.IMessage
 import org.aspectj.bridge.MessageHandler
 import org.aspectj.tools.ajc.Main
@@ -16,19 +13,7 @@ class AjxPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
 
-        def hasApp = project.plugins.withType(AppPlugin)
-        def hasLib = project.plugins.withType(LibraryPlugin)
-        if (!hasApp && !hasLib) {
-            throw new IllegalStateException("'android' or 'android-library' plugin required.")
-        }
-
-        final def log = project.logger
-        final def variants
-        if (hasApp) {
-            variants = project.android.applicationVariants
-        } else {
-            variants = project.android.libraryVariants
-        }
+        def config = new AjxConfig(project)
 
         project.repositories {
             google()
@@ -38,42 +23,24 @@ class AjxPlugin implements Plugin<Project> {
         project.dependencies {
             api 'org.aspectj:aspectjrt:1.8.10'
         }
-        // hugo https://github.com/JakeWharton/hugo/blob/master/hugo-runtime/build.gradle
 
-        variants.all { variant ->
+        final def log = project.logger
+
+        config.variants.all { variant ->
             if (!variant.buildType.isDebuggable()) {
                 log.debug("Skipping non-debuggable build type '${variant.buildType.name}'.")
                 return;
             }
 
-            //http://www.eclipse.org/aspectj/doc/released/devguide/ajc-ref.html
-            //
-            // -sourceRoots:
-            //  Find and build all .java or .aj source files under any directory listed in DirPaths. DirPaths, like classpath, is a single argument containing a list of paths to directories, delimited by the platform- specific classpath delimiter. Required by -incremental.
-            // -inpath:
-            //  Accept as source bytecode any .class files in the .jar files or directories on Path. The output will include these classes, possibly as woven with any applicable aspects. Path is a single argument containing a list of paths to zip files or directories, delimited by the platform-specific path delimiter.
-            // -classpath:
-            //  Specify where to find user class files. Path is a single argument containing a list of paths to zip files or directories, delimited by the platform-specific path delimiter.
-            // -aspectPath:
-            //  Weave binary aspects from jar files and directories on path into all sources. The aspects should have been output by the same version of the compiler. When running the output classes, the run classpath should contain all aspectpath entries. Path, like classpath, is a single argument containing a list of paths to jar files, delimited by the platform- specific classpath delimiter.
-            // -bootclasspath:
-            //  Override location of VM's bootclasspath for purposes of evaluating types when compiling. Path is a single argument containing a list of paths to zip files or directories, delimited by the platform-specific path delimiter.
-            // -d:
-            //  Specify where to place generated .class files. If not specified, Directory defaults to the current working dir.
-            // -preserveAllLocals:
-            //  Preserve all local variables during code generation (to facilitate debugging).
-
-            JavaCompile javaCompile = variant.hasProperty('javaCompiler') ? variant.javaCompiler : variant.javaCompile
+            JavaCompile javaCompile = (variant.hasProperty('javaCompiler') ? variant.javaCompiler : variant.javaCompile) as JavaCompile
             javaCompile.doLast {
-                String[] args = [
-                        "-showWeaveInfo",
-                        "-1.8",
-                        "-inpath", javaCompile.destinationDir.toString(),
-                        "-aspectpath", javaCompile.classpath.asPath,
-                        "-d", javaCompile.destinationDir.toString(),
-                        "-classpath", javaCompile.classpath.asPath,
-                        "-bootclasspath", project.android.bootClasspath.join(File.pathSeparator)
-                ]
+                String[] args = ["-showWeaveInfo",
+                                 "-1.8",
+                                 "-inpath", javaCompile.destinationDir.toString(),
+                                 "-aspectpath", javaCompile.classpath.asPath,
+                                 "-d", javaCompile.destinationDir.toString(),
+                                 "-classpath", javaCompile.classpath.asPath,
+                                 "-bootclasspath", config.bootClasspath.join(File.pathSeparator)]
                 log.debug "ajc args: " + Arrays.toString(args)
 
                 MessageHandler handler = new MessageHandler(true);
@@ -99,7 +66,30 @@ class AjxPlugin implements Plugin<Project> {
             }
         }
 
-        project.gradle.addListener(new TaskTimeListener())
-//        AppExtension android = project.extensions.getByType(AppExtension)
+//        project.afterEvaluate {
+//            config.variants.all { variant ->
+//                if (!variant.buildType.isDebuggable()) {
+//                    log.debug("Skipping non-debuggable build type '${variant.buildType.name}'.")
+//                    return;
+//                }
+//                JavaCompile javaCompile = (variant.hasProperty('javaCompiler') ? variant.javaCompiler : variant.javaCompile) as JavaCompile
+//                def ajxTask = project.tasks.create("ajxTask", AjxTask)
+//                ajxTask.sourceCompatibility = javaCompile.sourceCompatibility
+//                ajxTask.targetCompatibility = javaCompile.targetCompatibility
+//                ajxTask.encoding = javaCompile.options.encoding
+//                ajxTask.aspectPath = javaCompile.classpath.asList()
+//                ajxTask.outputDir = javaCompile.destinationDir.absolutePath
+//                ajxTask.classpath = javaCompile.classpath
+//                ajxTask.bootClasspath = config.bootClasspath.join(File.pathSeparator)
+//                ajxTask.inPath=javaCompile.destinationDir.listFiles()
+//
+//                ajxTask.dependsOn javaCompile
+//                javaCompile.finalizedBy ajxTask
+//            }
+//        }
+
+        if (project.plugins.hasPlugin(AppPlugin)) {
+            project.gradle.addListener(new TaskTimeListener())
+        }
     }
 }
